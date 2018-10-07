cf8130e19963e78a192d2a079e52d70cde0868e9
Administrator <249773304@qq.com>
2018-10-07 17:12:32 +0800

    根据json生成报告实例

--- src/main/java/skeleton/App.java
+++ src/main/java/skeleton/App.java
@@ -1,26 +1,46 @@
 package skeleton;
 
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.Reportable;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class App {
     public  static void main(String[] args){
-//        String input = "jakarta.apache.org";
-//        String pattern = "jakarta.apache.org";
-//        String sub = "${server}";
-        String input = "http://jakarta.apache.org/jmeter/index.html";
-        String pattern = "jakarta.apache.org";
-        String sub = "${server}";
+        //根据json生成报告
+        File reportOutputDirectory = new File("target");
+        List<String> jsonFiles = new ArrayList<>();
+        jsonFiles.add("D:\\projects\\cucumber-java-skeleton-master\\target\\124.json");
+        jsonFiles.add("D:\\projects\\cucumber-java-skeleton-master\\target\\dev.json");
 
-        StringBuilder ret = new StringBuilder(input.length());
-        int start = 0;
-        int index = -1;
-        final int length = pattern.length();
-        while ((index = input.indexOf(pattern, start)) >= start) {
-            ret.append(input.substring(start, index));
-            ret.append(sub);
-            start = index + length;
-        }
-        ret.append(input.substring(start));
-        System.out.print( ret.toString());
+        String buildNumber = "1";
+        String projectName = "cucumberProject";
+        boolean runWithJenkins = false;
+        boolean parallelTesting = false;
+
+        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+// optional configuration - check javadoc
+        configuration.setRunWithJenkins(runWithJenkins);
+        configuration.setBuildNumber(buildNumber);
+// addidtional metadata presented on main page
+        configuration.addClassifications("Platform", "Windows");
+        configuration.addClassifications("Browser", "Firefox");
+        configuration.addClassifications("Branch", "release/1.0");
+
+// optionally add metadata presented on main page via properties file
+//        List<String> classificationFiles = new ArrayList<>();
+//        classificationFiles.add("properties-1.properties");
+//        classificationFiles.add("properties-2.properties");
+//        configuration.addClassificationFiles(classificationFiles);
+
+        ReportBuilder reportBuilder = new ReportBuilder(jsonFiles, configuration);
+        Reportable result = reportBuilder.generateReports();
+// and here validate 'result' to decide what to do
+// if report has failed features, undefined steps etc
     }
 }
